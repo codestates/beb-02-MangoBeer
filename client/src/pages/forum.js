2353b7cad5c52e9314ce55973105bc9923fb4850
Seomingyun <aa123490@gmail.com>
2022-02-22 16:26:43 +0900

    [client][seo] modify forum page

--- client/src/pages/forum.js
+++ client/src/pages/forum.js
@@ -1,8 +1,26 @@
 // 글 조회 페이지
+import {Button } from 'react-bootstrap';
+
 function Forum() {
+
+    const createBntHandler = () => {
+      document.location.href = '/writeContents';
+    }
+
     return (
       <div className="Forum">
-        <h1>글 조회 페이지</h1>
+        <h2 style={{marginTop: "50px", marginLeft: "15%", textAlign: "left"}}>Forum</h2>
+
+        <div>
+          <span style={{marginTop: "10px", marginLeft: "15%", float: "left"}}>자유롭게 게시물을 작성하고 조회해 보세요!</span>
+          <Button 
+            style={{marginRight: "15%", float: "right"}}
+            variant="light" 
+            onClick={createBntHandler}
+          >
+            글 작성하기
+          </Button>
+        </div>
       </div>
     );
   }
