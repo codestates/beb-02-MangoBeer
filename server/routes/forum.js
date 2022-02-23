05cd2707d0d7f5f090b5c87bf04e40d516790bb8
WONSUNGJUN <adrwss2@gmail.com>
2022-02-23 18:02:58 +0900

    feat: 전체 글 보기 기능/데모 구현

--- server/routes/forum.js
+++ server/routes/forum.js
@@ -1,3 +1,17 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({  
+  host : '127.0.0.1',
+  user : 'root',
+  password : '1234',
+  port : 3307,
+  database : 'Mangobeer'
+})
+connection.connect();
+
 module.exports = async(req, res) => {
-    res.send("forum page...")
+    let sql = 'SELECT * FROM board'
+    connection.query(sql, (err, rows, fields) => {
+        res.json(rows);
+        console.log(rows);
+    })
 }
--- server/routes/write.js
+++ server/routes/write.js
@@ -1,3 +1,4 @@
+// mysql 설정 - 이거 라우터마다 설정해줘야하나?
 var mysql = require('mysql');
 var connection = mysql.createConnection({  
   host : '127.0.0.1',
@@ -7,6 +8,11 @@ var connection = mysql.createConnection({
   database : 'Mangobeer'
 })
 connection.connect();
+
+// web3 모듈 불러오기 - 보류
+// var Web3 = require('web3');
+// var web3 = new Web3('HTTP://127.0.0.1:7545')
+// web3.eth.getAccounts().then(accounts => console.log(accounts));
 
 module.exports = async(req, res) => {
     let title, content, username;
@@ -19,7 +25,7 @@ module.exports = async(req, res) => {
 
     let sql = 'insert into board (title, content, userName, created_at) values (?, ?, ?, NOW())';
     let params = [title, content, username];
-    connection.query(sql, params, function(err, rows, fields) {
+    connection.query(sql, params, (err, rows, fields) => {
         if(err) console.log(err);
     })
 }
