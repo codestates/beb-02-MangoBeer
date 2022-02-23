eacfb22917b7b6942f8d40eb3d326f49c96fca1c
WONSUNGJUN <adrwss2@gmail.com>
2022-02-23 22:13:44 +0900

    test: 전체 글 보기, 상세 글 보기 기능/데모 구현 전 임시 저장용 flag commit

--- server/routes/forum.js
+++ server/routes/forum.js
@@ -11,7 +11,14 @@ connection.connect();
 module.exports = async(req, res) => {
     let sql = 'SELECT * FROM board'
     connection.query(sql, (err, rows, fields) => {
-        res.json(rows);
-        console.log(rows);
+        // res.json(rows);
+        // console.log(rows);
+
+        console.log(rows.length)
+        for(let i=0; i<rows.length; i++) {
+          console.log(rows[i].title, rows[i].content, rows[i].userName ,rows[i].created_at)
+        }
+
+        res.send('<a href=http://localhost:4000/forum/id=' + rows[0].id + '>' +  "1." + rows[0].title + '</a>')
     })
 }
