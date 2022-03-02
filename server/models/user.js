7b43135a2e29164255a1f55132f7d6d77d8548ec
Seomingyun <aa123490@gmail.com>
2022-03-02 15:35:59 +0900

    [back][seo] modify DB schema for ERC20 token

--- server/models/user.js
+++ server/models/user.js
@@ -25,6 +25,10 @@ module.exports = (Sequelize, DataTypes) => {
         type: DataTypes.DATE,
         defaultValue: Sequelize.literal('now()') // 회원이 생길 때 자동으로 날짜가 등록이 됨
       },
+      tokenBalance: {
+        type: DataTypes.STRING(255),
+        allowNull: false,
+      }
     }, {
       timestamps: false,  // 생성일을 Sequelize가 자동으로 생성하지 말라는 옵션 
       underscored: true,   // Snake Case를 권장한다는 옵션
